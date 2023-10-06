6c589364eb2a809e5e9cdac2833e673c49b32cff
Peter Barry <pbarry@idbs.com>
2023-10-06 10:34:29 +0100

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,4 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This was added, again ssdsdsbeep dfdfdf"
+echo "This was added, again ssdsdssdsdsbeep dfdfdf"
